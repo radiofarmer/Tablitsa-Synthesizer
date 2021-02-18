8cbbf1c8b9d16788266abe590cb577265a4162c9
Bram Osterhout <bram.osterhout@gmail.com>
2021-02-18 21:54:28 +0100

    Deleted empty files

--- CustomControls.cpp
+++
@@ -1,8 +0,0 @@
-#include "CustomControls.h"
-
-template <int MAXNC>
-SequencerControl<MAXNC>::SequencerControl(const IRECT& bounds, const char* label, const IVStyle& style, int nSteps, EDirection dir) :
-  IVMultiSliderControl(bounds, label, style, nSteps, dir)
-{
-
-}
