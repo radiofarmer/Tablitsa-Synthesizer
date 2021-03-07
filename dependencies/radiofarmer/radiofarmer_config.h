ef97226bccc0055b745cd33021bdd89c293568bd
Bram Osterhout <bram.osterhout@gmail.com>
2021-03-07 08:19:52 +0100

    Added StereoBuffer struct to DSP library

--- dependencies/radiofarmer/radiofarmer_config.h
+++ dependencies/radiofarmer/radiofarmer_config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vectorclass.h>
+#include <algorithm>
 
 #define BEGIN_DSP_NAMESPACE namespace radiofarmer {
 #define END_DSP_NAMESPACE }
@@ -56,6 +57,85 @@ struct StereoSample
     l *= s;
     r *= s;
     return *this;
+  }
+
+  StereoSample& operator/(T s)
+  {
+    l /= s;
+    r /= s;
+    return *this;
+  }
+};
+
+template<typename T=sample_t>
+struct StereoBuffer
+{
+  T* const L;
+  T* const R;
+  const unsigned int N;
+
+  /** Create a buffer using explicit pointers to each channel.
+  @param l Pointer to the left channel buffer.
+  @param r Pointer to the right channel buffer.
+  @param nFrames Length of the buffer (in samples). If this is set to -1, the length will be 2^32 - 1, and arithmetic operations will not be available. */
+  StereoBuffer(T* l, T* r, const int nFrames=-1) : L(l), R(r), N(nFrames) {}
+
+  /* Create a buffer based on the address of the first sample, and the buffer length */
+  StereoBuffer(T* buf, const int nFrames) : L(buf), R(buf + nFrames), N(nFrames) {}
+
+  StereoBuffer& operator+(T* buf)
+  {
+    assert(static_cast<signed int>(nFrames) >= 0 && "Cannot add to buffer with an undefined length");
+    for (int i{ 0 }; i < nFrames; ++i)
+    {
+      L[i] += buf[0][i];
+      R[i] += buf[1][i];
+    }
+    return *this;
+  }
+
+  StereoBuffer& operator-(T* buf)
+  {
+    assert(static_cast<signed int>(nFrames) >= 0 && "Cannot subtract from buffer with an undefined length");
+    for (int i{ 0 }; i < nFrames; ++i)
+    {
+      L[i] -= buf[0][i];
+      R[i] -= buf[1][i];
+    }
+    return *this;
+  }
+
+  StereoBuffer& operator*(T* buf)
+  {
+    assert(static_cast<signed int>(nFrames) >= 0 && "Cannot multiply buffer with an undefined length");
+    for (int i{ 0 }; i < nFrames; ++i)
+    {
+      L[i] *= buf[0][i];
+      R[i] *= buf[1][i];
+    }
+    return *this;
+  }
+
+  StereoBuffer& operator/(T* buf)
+  {
+    assert(static_cast<signed int>(nFrames) >= 0 && "Cannot divide buffer with an undefined length");
+    for (int i{ 0 }; i < nFrames; ++i)
+    {
+      L[i] /= buf[0][i];
+      R[i] /= buf[1][i];
+    }
+    return *this;
+  }
+
+  template<class V=sample_v>
+  StereoSample<V> GetVector(const int offset = 0)
+  {
+    return StereoSample<V>(V().load(&L[offset]), V().load(&R[offset]));
+  }
+
+  StereoSample<T> operator[](const int idx)
+  {
+    return StereoSample<T>(L[idx], R[idx]);
   }
 };
 
