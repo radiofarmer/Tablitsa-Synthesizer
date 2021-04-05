bf926553f77656e32c7412494971613fae0cc422
Bram Osterhout <bram.osterhout@gmail.com>
2021-04-05 11:27:13 +0200

    Make preset loading compatible with wide character set

--- Presets.h
+++ Presets.h
@@ -1,3 +1,7 @@
-#define PRESET_NAMES {"Crust", "Cyanide", "Exotic Metals 1", "Free Radical", "Halogens 1", "Hydrochloric Acid", "Hydrofluoric Acid", "Isotonic", "Light Metals 1", "Meltdown", "Methane", "Nitrate", "Noble Metals", "Phosphate", "P Type", "Radon Leak", "Rare Earth Chimes", "Reductant", "Red Earth", "Spontaneous Decay", "Water", "White Paint", "Xenon Hexafluoride"}
+#define PRESET_NAMES {"Crust", "Cyanide", "Default", "Exotic Metals 1", "Free Radical", "Halogens 1", "Hydrochloric Acid", "Hydrofluoric Acid", "Isotonic", "Light Metals 1", "Meltdown", "Methane", "Nitrate", "Noble Metals", "Phosphate", "P Type", "Radon Leak", "Rare Earth Chimes", "Reductant", "Red Earth", "Spontaneous Decay", "Water", "White Paint", "Xenon Hexafluoride"}
 
-constexpr char* PRESET_ID_LIST[]{"Crust", "Cyanide", "Exotic_Metals_1", "Free_Radical", "Halogens_1", "Hydrochloric_Acid", "Hydrofluoric_Acid", "Isotonic", "Light_Metals_1", "Meltdown", "Methane", "Nitrate", "Noble_Metals", "Phosphate", "P_Type", "Radon_Leak", "Rare_Earth_Chimes", "Reductant", "Red_Earth", "Spontaneous_Decay", "Water", "White_Paint", "Xenon_Hexafluoride"};
+constexpr char* PRESET_ID_LIST[]{"Crust", "Cyanide", "Default", "Exotic_Metals_1", "Free_Radical", "Halogens_1", "Hydrochloric_Acid", "Hydrofluoric_Acid", "Isotonic", "Light_Metals_1", "Meltdown", "Methane", "Nitrate", "Noble_Metals", "Phosphate", "P_Type", "Radon_Leak", "Rare_Earth_Chimes", "Reductant", "Red_Earth", "Spontaneous_Decay", "Water", "White_Paint", "Xenon_Hexafluoride"};
+
+constexpr wchar_t* PRESET_ID_LIST_W[]{L"Crust", L"Cyanide", L"Default", L"Exotic_Metals_1", L"Free_Radical", L"Halogens_1", L"Hydrochloric_Acid", L"Hydrofluoric_Acid", L"Isotonic", L"Light_Metals_1", L"Meltdown", L"Methane", L"Nitrate", L"Noble_Metals", L"Phosphate", L"P_Type", L"Radon_Leak", L"Rare_Earth_Chimes", L"Reductant", L"Red_Earth", L"Spontaneous_Decay", L"Water", L"White_Paint", L"Xenon_Hexafluoride"};
+
+constexpr int N_PRESETS = 24;
--- scripts/make_default_presets.py
+++ scripts/make_default_presets.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 
   p_name = re.compile(r"\/((?!.*\/).*(?=.TPST))")
 
+  # Get all preset file names, without full paths
   for file in preset_files:
     file_new = file.replace("\\", "/").replace(" ", "_").replace("-", "_")
     os.rename(file, file_new)
@@ -16,10 +17,13 @@ if __name__ == "__main__":
   name_list = '#define PRESET_NAMES {"' + '", "'.join([pn.replace("_", " ") for pn in preset_names]) + '"}'
   id_list = '"' + '", "'.join(preset_names) + '"'
   id_var = 'constexpr char* PRESET_ID_LIST[]{' + id_list + '};'
-  print(id_var)
+
+  id_list_w = 'L"' + '", L"'.join(preset_names) + '"'
+  id_var_w = 'constexpr wchar_t* PRESET_ID_LIST_W[]{' + id_list_w + '};'
 
   with open("../Presets.h", "w") as f:
-    f.writelines([name_list, "\n\n", id_var])
+    f.writelines([name_list, "\n\n", id_var, "\n\n", id_var_w, "\n"])
+    f.write("\nconstexpr int N_PRESETS = {};\n".format(len(preset_names)))
 
   with open("preset_includes.txt", "w") as f:
-    f.writelines([pn + ' RCDATA "' + pf.replace('\\', '/') + '"\n' for pn, pf in zip(preset_names, preset_files)])
+    f.writelines([pn + ' RCDATA "' + os.path.abspath(pf).replace('\\', '/') + '"\n' for pn, pf in zip(preset_names, preset_files)])
